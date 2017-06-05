22e71bbcacbfe45b33e237b49da8a0a71d428c12
Christopher King <chris@coyfox.net>
2017-06-05 09:32:07 -0600

    Increased two key key-chord delay for 'jk' chord

--- lisp/setup-key-chord.el
+++ lisp/setup-key-chord.el
@@ -1,6 +1,8 @@
 (require 'key-chord)
 (key-chord-mode 1)
 
+(setq-default key-chord-two-keys-delay 0.5)
+
 (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
 
 (provide 'setup-key-chord)
