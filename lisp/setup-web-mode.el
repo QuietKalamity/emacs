e19b94140477515df8f55b342f9b098869112140
Christopher King <cking@backflipstudios.com>
2017-09-13 07:42:59 -0600

    Added flycheck for web-mode with html tidy and updated formatting

--- lisp/setup-web-mode.el
+++ lisp/setup-web-mode.el
@@ -1,9 +1,33 @@
+;;; setup-web-mode.el --- Summary
+;;;
+;;; Filename: setup-web-mode.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Commentary:
+
+(require 'company)
+(require 'flycheck)
 (require 'web-mode)
 
-(add-to-list 'auto-mode-alist '("\\.php?\\'" . web-mode))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Code:
 
-(setq web-mode-markup-indent-offset 2)
-(setq web-mode-css-indent-offset 2)
-(setq web-mode-code-indent-offset 2)
+(defun web-mode-setup ()
+  "Perform the web-mode environment setup."
+
+  ;; Setup the web-mode offsets
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+
+  ;; Use html-tidy for flycheck
+  (flycheck-add-mode 'html-tidy 'web-mode)
+  
+  )
+
+(add-hook 'web-mode-hook 'web-mode-setup)
 
 (provide 'setup-web-mode)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; setup-web-mode.el ends here
